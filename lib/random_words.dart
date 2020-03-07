707ac841ab455b6a3722a97497535c278efd7473
Jeferson GB <jefersongb@outlook.com.br>
2020-03-06 22:54:18 -0300

    Creates the favorite icon and the function of save favorite words

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -8,24 +8,40 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
+
   Widget _buildList(){
     return ListView.builder(
-  padding: const EdgeInsets.all(16),
-  itemBuilder: (context, item){
-    if(item.isOdd) return Divider();
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, item){
+        if(item.isOdd) return Divider();
 
-    final index = item ~/2;
+        final index = item ~/2;
     
-    if(index >= _randomWordPairs.length){
-      _randomWordPairs.addAll(generateWordPairs().take(10));
-    }
+        if(index >= _randomWordPairs.length){
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
     
-    return _buildRow(_randomWordPairs[index]);
-  },
-);
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
   }
   Widget _buildRow(WordPair pair){
-    return ListTile(title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)));
+    final alreadySaved = _savedWordPairs.contains(pair);
+
+    return ListTile(
+      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border, color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _savedWordPairs.remove(pair);
+          }else{
+            _savedWordPairs.add(pair);
+          }
+        });
+      },
+    );
   }
 
   Widget build(BuildContext context){
